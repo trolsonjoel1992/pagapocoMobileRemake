213bbad0f97f7f9f4e663ec0a8c23798c5e60263
LautaroRodriguez54 <lautaror5454@gmail.com>
2025-05-06 23:26:23 -0300

    folders

---
+++ src/app/_layout.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const Rootnavigation = () => {
+    return(
+    <View>
+        <Text>_layout</Text>
+    </View>
+    );
+};
+
+export default Rootnavigation;
