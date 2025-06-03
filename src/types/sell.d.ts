2f71df228a1beac8e13f66d0267dd2ba4a522d4d
CaptainShark007 <hugo.lautaro.brocal@gmail.com>
2025-06-03 13:20:10 -0300

    borrando archivos sin usar y renombrando otro

--- src/types/sell.d.ts
+++
@@ -1,8 +0,0 @@
-import { ImagePathKey } from '@/src/constants/ImagesPath'
-import { NavigationOptions } from 'expo-router/build/global-state/routing'
-
-export type Category = {
-  label: string
-  icon: ImagePathKey
-  route: NavigationOptions
-}
