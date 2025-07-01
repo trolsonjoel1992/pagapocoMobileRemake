372300cc8a4d17bb4b5af24d7856c78e41bbd91b
CaptainShark007 <hugo.lautaro.brocal@gmail.com>
2025-06-30 22:28:29 -0300

    correcciones en auntenticacion

--- src/app/(auth)/_layout.tsx
+++ src/app/(auth)/_layout.tsx
@@ -1,8 +1,16 @@
+import { Color } from '@/src/constants/colors'
 import { Stack } from 'expo-router'
 import React from 'react'
+import { StatusBar } from 'react-native'
 
 const AuthLayout = () => {
-  return <Stack screenOptions={{ headerShown: false }} />
+  return (
+    <>
+      <StatusBar backgroundColor={Color.primary} barStyle="light-content" />
+
+      <Stack screenOptions={{ headerShown: false }} />
+    </>
+  )
 }
 
 export default AuthLayout
